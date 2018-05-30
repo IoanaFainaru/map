9daa293082f146e2554d22c84c5fa81b0ccbb77e
Constantin Radu <radu17constantin@gmail.com>
2018-05-30 23:26:46 +0300

    1. update pom dependencies 2. add CORS origin url

--- src/main/java/fils/licenta/ioana/controller/UserController.java
+++ src/main/java/fils/licenta/ioana/controller/UserController.java
@@ -3,13 +3,11 @@ package fils.licenta.ioana.controller;
 import fils.licenta.ioana.model.UserModel;
 import fils.licenta.ioana.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/user")
 public class UserController {
